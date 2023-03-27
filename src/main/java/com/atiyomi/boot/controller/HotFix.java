8bde1c49d0291c9682c8d520da5d64b29ae9cdd7
YomiNoSleep <127716702+YomiNoSleep@users.noreply.github.com>
2023-03-27 16:55:51 +0800

    change on git hub

--- src/main/java/com/atiyomi/boot/controller/HotFix.java
+++ src/main/java/com/atiyomi/boot/controller/HotFix.java
@@ -4,5 +4,6 @@ public class HotFix {
     public static void main(String[] args) {
         System.out.println("this is the hot fix");
         System.out.println("this is the second change of the hot fix");
+        System.out.println("this is the change on git hub);
     }
 }
