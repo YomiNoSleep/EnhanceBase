822504068b0f10a9d1508f51024e775746708c98
Yomi <2639433152@qq.com>
2023-03-27 16:50:54 +0800

    forth commit of hotfix

--- src/main/java/com/atiyomi/boot/controller/UserController.java
+++ src/main/java/com/atiyomi/boot/controller/UserController.java
@@ -4,5 +4,6 @@ public class UserController {
     public static void main(String[] args) {
         System.out.println("Hello git");
         System.out.println("Hello git 2");
+        System.out.println("change on hot fix");
     }
 }
