f0ddb75ac1c0610aee9a2eb9052f71dd2c586e63
Yomi <2639433152@qq.com>
2023-03-27 16:36:12 +0800

    third commit

--- src/main/java/com/atiyomi/boot/controller/UserController.java
+++ src/main/java/com/atiyomi/boot/controller/UserController.java
@@ -3,5 +3,6 @@ package com.atiyomi.boot.controller;
 public class UserController {
     public static void main(String[] args) {
         System.out.println("Hello git");
+        System.out.println("Hello git 2");
     }
 }
