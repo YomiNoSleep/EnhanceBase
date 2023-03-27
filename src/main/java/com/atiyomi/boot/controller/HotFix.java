f95e2866ff5eab3adbb45bb7989a354b25d0da3a
Yomi <2639433152@qq.com>
2023-03-27 16:48:02 +0800

    second commit of hotfix

--- src/main/java/com/atiyomi/boot/controller/HotFix.java
+++ src/main/java/com/atiyomi/boot/controller/HotFix.java
@@ -3,5 +3,6 @@ package com.atiyomi.boot.controller;
 public class HotFix {
     public static void main(String[] args) {
         System.out.println("this is the hot fix");
+        System.out.println("this is the second change of the hot fix");
     }
 }
